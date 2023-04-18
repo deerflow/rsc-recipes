b049fd4390944c5778badc85e74ff3a69eed59c0
Florian <alu.florian@gmail.com>
2023-04-18 19:54:59 +0200

    feat: add blur placeholder for images, add no recipes found message

--- src/app/components/MealsList.tsx
+++ src/app/components/MealsList.tsx
@@ -2,8 +2,11 @@ import { Meal } from '@/types';
 import { FC } from 'react';
 import Image from 'next/image';
 
+const blurDataUrl =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAACJJREFUKFNjnDhx4n8GIgDjqEJ8oUR68DAxMTH8+/cPp6EA0PQd/6MyIfgAAAAASUVORK5CYII=';
+
 const MealsList: FC<Props> = ({ meals }) => {
-    if (!meals || !meals.length) return null;
+    if (!meals || !meals.length) return <p className='mt-24'>No recipes found</p>;
     if (meals.length === 1) {
         const meal = meals[0];
         return <Meal {...meal} />;
@@ -20,8 +23,16 @@ const MealsList: FC<Props> = ({ meals }) => {
 
 const Meal: FC<Meal> = ({ strMeal, strMealThumb, strCategory }) => {
     return (
-        <div className='m-4'>
-            <Image src={strMealThumb} alt={`Image of ${strMeal}`} width={250} height={250} className='rounded-2xl' />
+        <div className='m-4 w-[250px]'>
+            <Image
+                src={strMealThumb}
+                alt={`Image of ${strMeal}`}
+                width={250}
+                height={250}
+                className='rounded-2xl shadow-sm'
+                placeholder='blur'
+                blurDataURL={blurDataUrl}
+            />
             <h2 className='font-semibold text-xl mt-2'>{strMeal}</h2>
             <p className='text-gray-400'>{strCategory}</p>
         </div>
