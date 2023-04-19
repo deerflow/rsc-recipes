10f4dc4cdcec1908315bc232cd7379de6d6a6b04
Florian <alu.florian@gmail.com>
2023-04-19 11:14:15 +0200

    refacto: Clean up fns

--- src/fns.ts
+++ src/fns.ts
@@ -5,22 +5,21 @@ const MEALS_API_PATH = 'https://www.themealdb.com/api/json/v1/1';
 export const queryMeals = async (search?: string) => {
     if (!search) return querySelection();
     const response = await fetch(`${MEALS_API_PATH}/search.php?s=${search}`);
-    if (!response.ok) {
-        console.error(response);
-    }
+    if (!response.ok) return [];
     const data = await response.json();
-
     return data.meals as Meal[];
 };
 
 const querySelection = async () => {
     const response = await fetch(`${MEALS_API_PATH}/search.php?s`);
+    if (!response.ok) return [];
     const data = await response.json();
     return data.meals as Meal[];
 };
 
 export const queryMealById = async (id: string) => {
     const response = await fetch(`${MEALS_API_PATH}/lookup.php?i=${id}`);
+    if (!response.ok) return null;
     const data = await response.json();
     return data.meals as Meal[] | null;
 };
